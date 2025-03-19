74748f5eacd6640534776af177c8806745637b72
Yagyesh Ranjan Shukla <yshukla@griddynamics.com>
2025-03-19 13:42:54 +0530

    Fix backend port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const fs = require("fs").promises;
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Configure multer to store files in memory
 const upload = multer({ storage: multer.memoryStorage() });
